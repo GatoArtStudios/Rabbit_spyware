b8b1714d1b846950ac3d9b051333c7f1999381ba
GatoArtStudio <hackhcctecno@gmail.com>
2024-10-19 12:56:53 -0500

    agregando dependencia para utilizar gps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     testImplementation("junit:junit:4.13.2")
